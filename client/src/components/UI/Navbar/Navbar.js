cd419adf0494156fd4f48e716f11a5c6598b9395
Ayush Yadav <ayushyadav468@gmail.com>
2021-03-24 20:01:22 +0530

    frontend - minor style changes, backend - none

--- client/src/components/UI/Navbar/Navbar.js
+++ client/src/components/UI/Navbar/Navbar.js
@@ -3,19 +3,8 @@ import Searchbar from './Searchbar/Searchbar';
 import Menubar from './Menubar/Menubar';
 
 const Navbar = () => {
-	// const navBar = document.querySelector('#navBar');
-	// const banner = document.querySelector('#banner');
-	// console.log(navBar, banner);
-
-	// const options = {};
-	// const observer = new IntersectionObserver((entries, observer) => {
-	// 	console.log(entries);
-	// }, options);
-
-	// observer.observe(banner);
-
 	return (
-		<nav className={styles.navBar} id='navBar'>
+		<nav className={styles.navBar}>
 			<Searchbar />
 			<Menubar />
 		</nav>
