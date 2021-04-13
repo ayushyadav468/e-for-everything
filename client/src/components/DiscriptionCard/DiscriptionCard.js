05c2c0131f6d080e4462910cb25dadc1749196ee
Ayush Yadav <ayushyadav468@gmail.com>
2021-04-13 14:28:48 +0530

    frontend - login and register page made responsive, backend - none

--- client/src/components/DiscriptionCard/DiscriptionCard.js
+++ client/src/components/DiscriptionCard/DiscriptionCard.js
@@ -137,23 +137,36 @@ const DiscriptionCard = (props) => {
 							<strong>Rs.</strong> {product.productPrice}
 						</p>
 						<p className={styles.productRating}>
-							<strong>{product.rating}</strong>/5.0
+							{product.rating} / <strong>5.0</strong>
 						</p>
 					</div>
-					<QuantityBox
-						productQuantity={productQuantity}
-						setProductQuantity={setProductQuantity}
-						dialogBox={(messageToBeDisplayed) =>
-							dialogBox(messageToBeDisplayed)
-						}
-					/>
-					<div className={styles.productBtnDiv}>
+					<div className={styles.quantityBoxAndFavDiv}>
+						<QuantityBox
+							productQuantity={productQuantity}
+							setProductQuantity={setProductQuantity}
+							dialogBox={(messageToBeDisplayed) =>
+								dialogBox(messageToBeDisplayed)
+							}
+						/>
 						<button
-							className={[styles.productBtn, styles.productBuyNowBtn].join(' ')}
+							className={styles.fav}
 							onClick={addProductToFavouriteHandler}
 						>
-							Favourites
+							<svg
+								width='512'
+								height='431'
+								viewBox='0 0 512 431'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path
+									d='M469.585 39.7395C413.906 -13.2445 323.279 -13.2445 267.582 39.7275L256.001 50.7275L244.432 39.7325C188.729 -13.2465 98.114 -13.2405 42.411 39.7325C15.061 65.7465 0 100.251 0 136.881C0 173.523 15.061 208.023 42.415 234.048L243.634 425.081C247.095 428.375 251.551 430.014 256 430.014C260.449 430.014 264.899 428.367 268.366 425.074L469.585 234.048C496.933 208.029 512 173.524 512 136.887C512 100.251 496.933 65.7525 469.585 39.7395ZM444.829 207.998L256 387.277L67.177 208.004C47.026 188.842 35.93 163.583 35.93 136.887C35.93 110.191 47.026 84.9325 67.177 65.7705C88.196 45.7695 115.802 35.7755 143.414 35.7755C171.032 35.7755 198.65 45.7815 219.669 65.7825L243.622 88.5325C250.556 95.1255 261.437 95.1255 268.372 88.5325L292.331 65.7705C334.375 25.7745 402.779 25.7745 444.823 65.7705C464.968 84.9335 476.07 110.191 476.07 136.887C476.07 163.583 464.968 188.836 444.829 207.998Z'
+									fill='#3A3A3A'
+								/>
+							</svg>
 						</button>
+					</div>
+					<div className={styles.productBtnDiv}>
 						<button
 							className={[styles.productBtn, styles.productAddToCartBtn].join(
 								' '
@@ -161,6 +174,13 @@ const DiscriptionCard = (props) => {
 							onClick={addProductToCartHandler}
 						>
 							Add to Cart
+						</button>
+						<button
+							className={[styles.productBtn, styles.productBuyNowBtn].join(' ')}
+							style={{ cursor: 'not-allowed' }}
+							disabled
+						>
+							Buy Now
 						</button>
 					</div>
 				</div>
