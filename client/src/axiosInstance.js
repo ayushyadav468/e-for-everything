cdb88ca5c2be9e78cc7d2fbf865c3cecaf1d80b5
Ayush Yadav <ayushyadav468@gmail.com>
2021-05-09 08:46:12 +0530

    axios instance updated for deployment

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
 
 const port = process.env.PORT || 8000;
+
+const configs = {
+	development: {
+		SERVER_URI: `http://localhost:${port}`,
+	},
+	production: {
+		SERVER_URI: 'https://e-for-everything.herokuapp.com',
+	},
+};
+
 const instance = axios.create({
-	baseURL: 'http://localhost:' + port,
+	baseURL: `${configs[process.env.NODE_ENV].SERVER_URI}`,
 });
 
 export default instance;
