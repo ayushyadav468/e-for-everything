f11571129f4ac018689bc4138f9c13e0ce08b8a1
Ayush Yadav <ayushyadav468@gmail.com>
2021-05-14 13:46:19 +0530

    frontend - none, backend - user routes updated

--- src/Routes/public/userRoutes.js
+++ src/Routes/public/userRoutes.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const router = require('express').Router();
 const User = require('../../Models/UserModel');
+const bcrypt = require('bcrypt');
 const {
 	loginValidation,
 	registerValidation,
 } = require('../../Validation/UserValidation');
 
-router.post('/login', (req, res) => {
+// User Login Route
+router.post('/login', async (req, res) => {
+	// Validation
 	const { error } = loginValidation(req.body);
 	if (error)
 		return res
@@ -15,75 +18,79 @@ router.post('/login', (req, res) => {
 
 	const userEmail = req.body.email;
 	const userPassword = req.body.password;
-	User.findOne({ email: userEmail, password: userPassword })
-		.select('-__v -dateAdded -password')
-		.exec()
-		.then((user) => {
-			if (user) {
-				const response = {
-					...user._doc,
-				};
-				res.status(200).json(response);
-			} else {
-				res.status(404).json({
-					error: { message: 'Email or password is wrong' },
-				});
-			}
-		})
-		.catch((err) => {
-			console.log(
-				'Error in finding user in get request of user routes ' + err.message
-			);
-			res.status(500).json(err);
-		});
+	try {
+		// Check if user exist
+		const loginUser = await User.findOne({ email: userEmail });
+		if (!loginUser)
+			return res.status(400).json({ error: { message: 'Email not valid' } });
+
+		// check password
+		const validatePassword = await bcrypt.compare(
+			userPassword,
+			loginUser.password
+		);
+		if (!validatePassword)
+			return res.status(400).json({ error: { message: 'Password not valid' } });
+		const response = {
+			_id: loginUser._doc._id,
+			firstName: loginUser._doc.firstName,
+			lastName: loginUser._doc.lastName,
+			email: loginUser._doc.email,
+			seller: loginUser._doc.seller,
+		};
+		res.status(200).json(response);
+	} catch (err) {
+		console.log('Error in logging user ' + err.message);
+		res.status(500).json({ error: { message: err.message } });
+	}
 });
 
-router.post('/register', (req, res) => {
-	const emailToBeChecked = req.body.email;
-	User.findOne({ email: emailToBeChecked })
-		.exec()
-		.then((user) => {
-			if (user) {
-				const response = {
-					message: 'Email already registered',
-				};
-				res.status(208).json(response);
-			} else {
-				const { error } = registerValidation(req.body);
-				if (error)
-					return res
-						.status(400)
-						.json({ error: { message: error.details[0].message } });
+// User Register Route
+router.post('/register', async (req, res) => {
+	// Validation
+	const { error } = registerValidation(req.body);
+	if (error)
+		return res
+			.status(400)
+			.json({ error: { message: error.details[0].message } });
 
-				const user = new User({
-					_id: new mongoose.Types.ObjectId(),
-					firstName: req.body.firstName,
-					lastName: req.body.lastName,
-					email: req.body.email,
-					password: req.body.password,
-					seller: req.body.seller,
-				});
-				user
-					.save()
-					.then((user) => {
-						const response = {
-							_id: user._doc._id,
-							firstName: user._doc.firstName,
-							lastName: user._doc.lastName,
-							email: user._doc.email,
-							seller: user._doc.seller,
-						};
-						res.status(200).json(response);
-					})
-					.catch((err) => {
-						console.log('Error in saving user ' + err.message);
-						res.status(500).json(err);
-					});
-			}
-		})
-		.catch((err) => {
-			console.log('Error in finding duplicate email' + err);
+	// Check if email exist
+	const emailToBeChecked = req.body.email;
+	const emailExist = await User.findOne({ email: emailToBeChecked });
+	if (emailExist)
+		return res.status(208).json({
+			error: { message: 'Email already registered' },
 		});
+
+	// Hasing password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	//Creating user
+	const newUser = new User({
+		_id: new mongoose.Types.ObjectId(),
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: hashedPassword,
+		seller: req.body.seller,
+	});
+
+	// Saving user
+	try {
+		const savedUser = await newUser.save();
+		const response = {
+			_id: savedUser._doc._id,
+			firstName: savedUser._doc.firstName,
+			lastName: savedUser._doc.lastName,
+			email: savedUser._doc.email,
+			seller: savedUser._doc.seller,
+		};
+		res.status(200).json(response);
+	} catch (err) {
+		console.log('Error in saving user ' + err.message);
+		res.status(500).json({ error: { message: err.message } });
+	}
 });
 
 module.exports = router;
